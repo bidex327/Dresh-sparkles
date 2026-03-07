b1e4ff75521981720911824ef882e5d7e481097d
bidex327 <adebayobidemi303@gmail.com>
2026-03-07 14:43:51 +0100

    Update

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://wa.me/2290191463171"
+      href="https://wa.me/+22991402522"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 p-4 rounded-full shadow-2xl hover:scale-110 transition z-50"
